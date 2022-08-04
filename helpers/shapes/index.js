40df9209d9896f08118bc054d2d0cba4c9b139ff
Alexandre Kings <alexandrekings1@gmail.com>
2022-08-04 22:32:11 +0100

    Add base files for the generation of OBB for train tracks.

--- helpers/shapes/index.js
+++ helpers/shapes/index.js
@@ -6,3 +6,21 @@ const h = canvas.height
 
 ctx.fillStyle = '#DDDDFF'
 ctx.fillRect(0,0,w,h)
+
+class Vector2D{
+    constructor(x,y){
+        this.x = x
+        this.y = y
+    }
+
+    // Returns result of the add operation
+    add(v){
+        return new Vector2D(this.x + v.x, this.y + v.y)
+    }
+
+    // Returns result of the dot operation
+    dot(v){
+        
+    }
+
+}
