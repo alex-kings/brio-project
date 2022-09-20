99ed222515bae883e84acf207fc37559ce6a9c77
atk3 <atk3@st-andrews.ac.uk>
2022-09-20 18:49:36 +0100

    Print width with 2d Bezier curve.

--- helpers/beziers/index.js
+++ helpers/beziers/index.js
@@ -5,17 +5,20 @@ class vec2d{
         this.y = y
     }
     add(v){
-        this.x += v.x
-        this.y += v.y
+        return new vec2d(this.x + v.x, this.y + v.y)
+    }
+    subtract(v){
+        return new vec2d(this.x - v.x, this.y - v.y)
     }
     dot(v){
-        this.x *= v.x
-        this.y *= v.y
+        return new vec2d(this.x * v.x, this.y * v.y)
     }
     scale(a){
-        this.x *= a
-        this.y *= a
+        return new vec2d(this.x * a, this.y * a)
     }
+    modulus(){
+        return Math.sqrt(this.x**2 + this.y**2)
+    }   
 }
 
 
@@ -29,13 +32,34 @@ const example2 = [new vec2d(10,40), new vec2d(600,120), new vec2d(200,450)]
 const example3 = [new vec2d(10,40), new vec2d(600,120), new vec2d(200,450), new vec2d(600,200)]
 
 
-// Print on screen a linear bezier curve
-function print2dBezier(points){
+// Print on screen a linear bezier curve with width
+function print2dBezier(points, width){
     // Check that we have 2 points
     if(points.length != 2) return
+    ctx.strokeStyle = 'red' // Plot red line
     ctx.beginPath()
     ctx.moveTo(points[0].x, points[0].y)
     ctx.lineTo(points[1].x, points[1].y)
+    ctx.stroke()
+
+    // Calculate unit perpendicular
+    let unitPerpendicular = points[1].subtract(points[0])
+    unitPerpendicular = new vec2d(unitPerpendicular.y, -unitPerpendicular.x)
+    unitPerpendicular = unitPerpendicular.scale(20/unitPerpendicular.modulus())
+
+    // Plot edges of rectangle
+    let e1 = points[0].add(unitPerpendicular)
+    let e2 = points[0].subtract(unitPerpendicular)
+    let e3 = points[1].subtract(unitPerpendicular)
+    let e4 = points[1].add(unitPerpendicular)
+
+    ctx.strokeStyle = 'black'
+    ctx.beginPath()
+    ctx.moveTo(e1.x, e1.y)
+    ctx.lineTo(e2.x, e2.y)
+    ctx.lineTo(e3.x, e3.y)
+    ctx.lineTo(e4.x, e4.y)
+    ctx.lineTo(e1.x, e1.y)
     ctx.stroke()
 }
 
@@ -72,5 +96,5 @@ function print4dBezier(points){
 
 
 print2dBezier(example)
-print3dBezier(example2)
-print4dBezier(example3)
+// print3dBezier(example2)
+// print4dBezier(example3)
