7a22ca1a8a93dbd9c4579946ac21210835ab98b3
alex-kings <alexandrekings1@gmail.com>
2022-10-22 18:40:58 +0100

    Initialise meson to build cpp.

--- backend/src/core/main.cpp
+++ backend/src/core/main.cpp
@@ -1,11 +1,27 @@
 #include <iostream>
 #include <json/json.h>
-#include <fstream>
+#include <memory>
 
 
-int main(int argc, char** argv) {
+Json::Value getJson(std::string rawJson) {
+
+    const auto rawJsonLength = static_cast<int>(rawJson.length());
+    JSONCPP_STRING err;
     Json::Value root;
+
     Json::CharReaderBuilder builder;
+    const std::unique_ptr<Json::CharReader> reader(builder.newCharReader());
+    if (!reader->parse(rawJson.c_str(), rawJson.c_str() + rawJsonLength, &root,
+                       &err)) {
+        std::cout << "error" << std::endl;
+        return NULL;
+    }
+
+    return root;
+}
+
+int main(int argc, char **argv) {
+    std::cout << "starting";
 
     
 }
