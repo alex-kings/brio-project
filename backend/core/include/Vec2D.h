38bbf013f95dd1aa286332f3862de4707b1c73ec
atk3 <atk3@st-andrews.ac.uk>
2022-10-25 17:01:06 +0100

    Implement basic functions of Vec2D class.

--- backend/core/include/Vec2D.h
+++ backend/core/include/Vec2D.h
@@ -1,13 +1,35 @@
-#ifndef VEC2D_H
-#define VEC2D_H
+#pragma once
+#include <cmath>
 
 class Vec2D {
 private:
-    float x;
-    float y;
-public:
-    Vec2D(double x, double y);
-    void add(Vec2D v);
-};
+    float a;
+    float b;
 
-#endif
+public:
+
+    Vec2D(double x, double y) {
+        a = x;
+        b = y;
+    }
+
+    float getX() {return a;}
+    float getY() {return b;}
+
+    // Adds the given vector to this vector.
+    void add(Vec2D v) {
+        a += v.a;
+        b += v.b;
+    }
+
+    // Dots this vector with the given vector.
+    void dot(Vec2D v) {
+        a *= v.a;
+        b *= v.b;
+    }
+
+    // Return the modulus of this vector.
+    float getModulus() {
+        return std::sqrt(a*a + b*b);
+    }
+};
--- backend/core/src/Vec2D.cpp
+++ backend/core/src/Vec2D.cpp
@@ -3,13 +3,3 @@
 /**
  * 2D vector class.
 */
-
-Vec2D::Vec2D(double a, double b) {
-    x = a;
-    y = b;
-}
-
-void Vec2D::add(Vec2D v){
-    x += v.x;
-    y += v.y;
-}
