bc8a7e2aef3ebc9871f134852c3955256f713c42
atk3 <atk3@st-andrews.ac.uk>
2022-10-25 17:55:10 +0100

    Move rotation function from header to cpp file.

--- backend/core/include/Vec2D.h
+++ backend/core/include/Vec2D.h
@@ -36,12 +36,5 @@ public:
     }
 
     // Rotates this point around the given rotation point.
-    void rotate(const Vec2D& rotationPoint, float angle) {
-        float cAngle = std::cos(angle);
-        float sAngle = std::sin(angle);
-
-        float tempa = cAngle * rotationPoint.a + sAngle * rotationPoint.b;
-        b = -sAngle * rotationPoint.a + cAngle * rotationPoint.b;
-        a = tempa;
-    }
+    void rotate(const Vec2D& rotationPoint, float angle);    
 };
--- backend/core/src/Vec2D.cpp
+++ backend/core/src/Vec2D.cpp
@@ -1,5 +1,12 @@
 #include "Vec2D.h"
 
-/**
- * 2D vector class.
-*/
+
+// Rotates this point around the given rotation point.
+void Vec2D::rotate(const Vec2D& rotationPoint, float angle) {
+    float cAngle = std::cos(angle);
+    float sAngle = std::sin(angle);
+
+    float tempa = cAngle * rotationPoint.a + sAngle * rotationPoint.b;
+    b = -sAngle * rotationPoint.a + cAngle * rotationPoint.b;
+    a = tempa;
+}
