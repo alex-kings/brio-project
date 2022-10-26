139f900f0d5516f68259edb9b2668969383f31b5
atk3 <atk3@st-andrews.ac.uk>
2022-10-26 17:39:57 +0100

    Add level to Part.

--- backend/core/src/Part.cpp
+++ backend/core/src/Part.cpp
@@ -1,7 +1,11 @@
 #include "Part.h"
 
 Part::Part(const Json::Value& jsonRep) {
+    // Add obbs
     for(const Json::Value& rectangle : jsonRep["rectanlges"]) {
         obbs.push_back(Obb(rectangle));
     }
+
+    // Add level
+    level = jsonRep["level"].asInt();
 }
