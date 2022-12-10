a37b3fb8ac9107c31c29cf1db31145eb89ed32d3
atk3 <atk3@st-andrews.ac.uk>
2022-12-10 17:53:43 +0000

    Add multithreading test.

--- backend/core/src/main.cpp
+++ backend/core/src/main.cpp
@@ -7,37 +7,59 @@
 #include "Vec2D.h"
 #include "Track.h"
 
+#include <thread>
+
 #include <fstream>
 
-int main(int argc, char* argv[]) {
 
-    std::cerr << "Start" << "\n";
-
-    // Get input from node program
-    std::string input = argv[1];
-
-
-    // TEST
-    // std::string input = "{\"E\":\"8\",\"A\":\"6\",\"E1\":\"4\",\"D\":\"2\"}";
-
-
-    // User selection of pieces and their available quantities.
-    Json::Value pieceSelection;
-    try {
-        // Example
-        pieceSelection = readJson(input);
-    }
-    catch(const std::domain_error& e) {
-        std::cerr << e.what() << std::endl;
-    }
-
-    // Get track
-    // Piece startPiece = getTrack(pieceSelection);
-
-    Track t(pieceSelection);
+void printHello(int i) {
+    std::cout << "Hello" << i << "\n";
+    std::cout << "Hello" << i << "\n";
+    std::cout << "Hello" << i << "\n";
+    std::cout << "Hello" << i << "\n";
 }
 
 
+
+
+int main(int argc, char* argv[]) {
+
+    // std::cerr << "Start" << "\n";
+
+    // // Get input from node program
+    // std::string input = argv[1];
+
+
+    // // TEST
+    // // std::string input = "{\"E\":\"8\",\"A\":\"6\",\"E1\":\"4\",\"D\":\"2\"}";
+
+
+    // // User selection of pieces and their available quantities.
+    // Json::Value pieceSelection;
+    // try {
+    //     // Example
+    //     pieceSelection = readJson(input);
+    // }
+    // catch(const std::domain_error& e) {
+    //     std::cerr << e.what() << std::endl;
+    // }
+
+    // // Get track
+    // // Piece startPiece = getTrack(pieceSelection);
+
+    // Track t(pieceSelection);
+
+
+    // TEST MULTITHREADING
+
+    uint maxThreads = std::thread::hardware_concurrency()*2; // 2 times the number of available cores.
+    std::thread threads[maxThreads];
+    for (uint i = 0; i < maxThreads; i++) {
+        threads[i] = std::thread(printHello, i);
+        threads[i].join();
+    }
+
+}
 
 
 
