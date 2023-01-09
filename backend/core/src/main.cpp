6f02fd0d28ef3693831d8f2db15988bd88f17007
atk3 <atk3@st-andrews.ac.uk>
2023-01-09 19:25:53 +0000

    Add wasm generation.

--- backend/core/src/main.cpp
+++ backend/core/src/main.cpp
@@ -22,18 +22,58 @@ void printHello(int i) {
     std::cout << "Hello" << i << "\n";
 }
 
-int main(int, char* argv[]) {
+// int main(int, char* argv[]) {
 
-    std::cerr << "Start" << "\n";
+//     std::cerr << "Start" << "\n";
 
-    // Get input from node program
-    std::string input = argv[1];
+//     // Get input from node program
+//     std::string input = argv[1];
 
-    // TEST
-    // std::string input = "{\"E\":\"8\",\"A\":\"6\",\"E1\":\"4\",\"D\":\"2\"}";
+//     // TEST
+//     // std::string input = "{\"E\":\"8\",\"A\":\"6\",\"E1\":\"4\",\"D\":\"2\"}";
 
 
+//     // User selection of pieces and their available quantities.
+//     Json::Value pieceSelection;
+//     try {
+//         // Example
+//         pieceSelection = readJson(input);
+//     }
+//     catch(const std::domain_error& e) {
+//         std::cerr << e.what() << std::endl;
+//     }
+
+//     Json::Value ressources = getPieceRessources(); // Library of pieces
+//     std::vector<Piece> pieces;
+//     for(const std::string& member : pieceSelection.getMemberNames()) {
+//         for(int i = 0; i < std::stoi(pieceSelection[member].asString()); i++) {
+//             // Add Piece to available pieces.
+//             pieces.emplace_back(ressources[member]);
+//         }
+//     }
+
+//     // Get track
+//     // Piece startPiece = getTrack(pieceSelection);
+
+//     Track t(pieces);
+
+
+//     // TEST MULTITHREADING
+
+//     // unsigned int maxThreads = std::thread::hardware_concurrency()*2; // 2 times the number of available cores.
+//     // std::thread threads[maxThreads];
+//     // for (unsigned int i = 0; i < maxThreads; i++) {
+//     //     threads[i] = std::thread(printHello, i);
+//     // }
+//     // for(unsigned int i = 0; i < maxThreads; i++) {
+//     //     threads[i].join();
+//     // }
+// }
+
+std::string generateTrack(const std::string& selection) {
     // User selection of pieces and their available quantities.
+    std::cout << "Hello" << "\n";
+    std::string input = "{\"E\":\"8\",\"A\":\"6\",\"E1\":\"4\",\"D\":\"2\"}";
     Json::Value pieceSelection;
     try {
         // Example
@@ -53,25 +93,8 @@ int main(int, char* argv[]) {
     }
 
     // Get track
-    // Piece startPiece = getTrack(pieceSelection);
-
     Track t(pieces);
-
-
-    // TEST MULTITHREADING
-
-    // unsigned int maxThreads = std::thread::hardware_concurrency()*2; // 2 times the number of available cores.
-    // std::thread threads[maxThreads];
-    // for (unsigned int i = 0; i < maxThreads; i++) {
-    //     threads[i] = std::thread(printHello, i);
-    // }
-    // for(unsigned int i = 0; i < maxThreads; i++) {
-    //     threads[i].join();
-    // }
-}
-
-std::string generateTrack(const std::string& pieceSelection) {
-    return "Track generated";
+    return "generated";
 }
 
 /**
