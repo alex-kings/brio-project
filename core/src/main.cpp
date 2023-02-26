bd4a4474c9c24174091a1b670e35122bba8b6e1d
atk3 <atk3@st-andrews.ac.uk>
2023-02-26 19:28:59 +0000

    Fix stupid bug.

--- core/src/main.cpp
+++ core/src/main.cpp
@@ -15,62 +15,55 @@
 // using namespace emscripten;
 
 
-void printHello(int i) {
-    std::cout << "Hello" << i << "\n";
-    std::cout << "Hello" << i << "\n";
-    std::cout << "Hello" << i << "\n";
-    std::cout << "Hello" << i << "\n";
-}
+// int main() {
+//     // Test pieces.
+//     std::string input = "{ \"E\": \"12\" }";
 
-int main() {
-    // Test pieces.
-    std::string input = "{ \"E\": \"12\" }";
+//     // User selection of pieces and their available quantities.
+//     Json::Value pieceSelection;
+//     try {
+//         // Example
+//         pieceSelection = readJson(input);
+//     }
+//     catch(const std::domain_error& e) {
+//         std::cerr << e.what() << std::endl;
+//     }
 
-    // User selection of pieces and their available quantities.
-    Json::Value pieceSelection;
-    try {
-        // Example
-        pieceSelection = readJson(input);
-    }
-    catch(const std::domain_error& e) {
-        std::cerr << e.what() << std::endl;
-    }
+//     Json::Value ressources = getPieceRessources(); // Library of pieces
+//     std::vector<Piece> pieces;
+//     for(const std::string& member : pieceSelection.getMemberNames()) {
+//         for(int i = 0; i < std::stoi(pieceSelection[member].asString()); i++) {
+//             // Add Piece to available pieces.
+//             pieces.emplace_back(ressources[member]);
+//         }
+//     }
 
-    Json::Value ressources = getPieceRessources(); // Library of pieces
-    std::vector<Piece> pieces;
-    for(const std::string& member : pieceSelection.getMemberNames()) {
-        for(int i = 0; i < std::stoi(pieceSelection[member].asString()); i++) {
-            // Add Piece to available pieces.
-            pieces.emplace_back(ressources[member]);
-        }
-    }
+//     // Create output file
+//     std::ofstream f;
+//     std::time_t now = std::time(0);
+//     std::string dt = std::ctime(&now);
+//     f.open("timing_" + dt + ".csv");
 
-    // Create output file
-    std::ofstream f;
-    std::time_t now = std::time(0);
-    std::string dt = std::ctime(&now);
-    f.open("timing_" + dt + ".csv");
+//     // Test building the track for seeds 1 to 100
+//     for(int i = 1; i < 101; i++) {
+//         std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
 
-    // Test building the track for seeds 1 to 100
-    for(int i = 1; i < 101; i++) {
-        std::chrono::steady_clock::time_point startTime = std::chrono::steady_clock::now();
+//         // Create circuit ...
+//         Circuit track(pieces, i, false, "close");
+//         if(!track.generate()) {
+//             // Couldn't generate track
+//             f << "-1,";
+//             continue;
+//         }
 
-        // Create circuit ...
-        Circuit track(pieces, i, false, "close");
-        if(!track.generate()) {
-            // Couldn't generate track
-            f << "-1,";
-            continue;
-        }
+//         // Time of execution
+//         std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
+//         int elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
+//         // Add execution time to the list.
+//         f << elapsed << ",";
+//     }
 
-        // Time of execution
-        std::chrono::steady_clock::time_point endTime = std::chrono::steady_clock::now();
-        int elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(endTime - startTime).count();
-        // Add execution time to the list.
-        f << elapsed << ",";
-    }
-
-}
+// }
 
 std::string generateTrack(const std::string& selection, const int seed, const int isTwoLevel, const std::string vCondition) {
     // User selection of pieces and their available quantities.
